9044c1ddbbccee9204d7f9c115444a115c93d7cb
Eric Bergan <bootcamp.ejb@gmail.com>
2019-08-28 08:56:21 -0500

    moved images
    trying to get them to show on deployed site

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ import Project from './Project/Project'
 const projects = [
   {
     name: "Burger",
-    imageURL: "./images/burger.JPG",
+    imageURL: "/burger.JPG",
     description: "Full stack app that adds the user's input (ideally a sandwich) to a MySQL database, allows the user to 'devour' the sandwich, and then delete it",
     runApp: "https://ericjbergan.github.io/burger.github.io/",
     seeCode: "https://github.com/ericjbergan/burger.github.io"
   },
   {
     name: "Nesterly",
-    imageURL: "./images/nesterly.JPG",
+    imageURL: "/nesterly.JPG",
     description: "Full stack app that allows you to keep track of your household goods; functions but is still a work in progress",
     runApp: "https://bootcamp-proj2.herokuapp.com/",
     seeCode: "https://github.com/ericjbergan/bootcamp-project2.github.io"
   },
   {
     name: "Gem Collector",
-    imageURL: "./images/gems.JPG",
+    imageURL: "/gems.JPG",
     description: "A game for improving basic math and memory skills",
     runApp: "https://ericjbergan.github.io/crystal-collector.github.io/",
     seeCode: "https://github.com/ericjbergan/crystal-collector.github.io"
@@ -29,27 +29,28 @@ const projects = [
 
 class App extends Component {
 state = {
- isAboutMe: false
+ isAboutMe: true
 }
 
 switchAboutMe = () => {
   //update state
   console.log('I was clicked');
   this.setState({
-    isAboutMe: true
+    isAboutMe: false
   })
 }
 
   render() {
     return (
       <div className="App container">
-        <Navbar 
-          onClick={this.switchAboutMe} 
+        <Navbar onClick={this.switchAboutMe} 
           />
         <div id="body">
           {this.state.isAboutMe ? 
-          <h1>Hi, I'm Eric; Full Stack Developer</h1>
-          //<h1>from Minneapolis/St. Paul, MN</h1>
+          <div>
+            <h1>Hi, I'm Eric; Full Stack Developer</h1>
+            <h1>from Minneapolis/St. Paul, MN</h1>
+          </div>
           :
           <h1>THIS IS WORKING!!!!</h1>
         }
