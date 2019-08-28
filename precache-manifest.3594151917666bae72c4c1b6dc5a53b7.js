377183cb2e221a694e5c2400fd7c53eaf97f5ab6
Eric Bergan <bootcamp.ejb@gmail.com>
2019-08-28 08:26:26 -0500

    Updates

---
+++ precache-manifest.3594151917666bae72c4c1b6dc5a53b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fa3cfebd1e2488378a6c89174cf6d2a",
+    "url": "/portfolio2.0/index.html"
+  },
+  {
+    "revision": "d0cb95fccfef88a8dcb5",
+    "url": "/portfolio2.0/static/css/main.fa082be9.chunk.css"
+  },
+  {
+    "revision": "909f17b3cb988c86fbfa",
+    "url": "/portfolio2.0/static/js/2.23715905.chunk.js"
+  },
+  {
+    "revision": "d0cb95fccfef88a8dcb5",
+    "url": "/portfolio2.0/static/js/main.b7f276e3.chunk.js"
+  },
+  {
+    "revision": "f2c1994e2c988561eef5",
+    "url": "/portfolio2.0/static/js/runtime~main.01bafd33.js"
+  }
+]);
