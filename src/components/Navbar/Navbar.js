6619fa6eb18e59ed825785fc3f553a44021805f3
Eric Bergan <bootcamp.ejb@gmail.com>
2019-10-24 16:59:36 -0500

    minor challenges
    and adding bootcamp badge

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,19 +12,22 @@ const navbar = (props) => {
           <h2 className="navTitle">Full Stack Developer</h2>
         </div>
         <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav links">
-          <li className="nav-item">
-            <p className="nav-link" onClick={props.click}>About Me</p>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="https://github.com/ericjbergan" target="_blank" rel="noopener noreferrer">Github</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="https://www.linkedin.com/in/eric-bergan/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href = "mailto: ericjbergan@gmail.com">Contact</a>
-          </li>
+          <ul className="navbar-nav links">
+            <li className="nav-item">
+              <p className="nav-link" onClick={props.click}>About Me</p>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="https://github.com/ericjbergan" target="_blank" rel="noopener noreferrer">Github</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="https://www.linkedin.com/in/eric-bergan/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="https://drive.google.com/file/d/1M0arQXR9ouHr8fAeIFoCUsrKBQFOf7oS/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="mailto: ericjbergan@gmail.com">Contact</a>
+            </li>
           </ul>
         </div>
       </nav>
